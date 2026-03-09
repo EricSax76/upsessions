262fc198c23b110747582e7cab1c3bb493998f2a
Eric Moscoso <ericmoscoso76@hotmail.com>
2026-03-09 08:10:37 +0100

    orphan widgets clen up

--- lib/modules/groups/ui/widgets/groups_widgets.dart
+++ lib/modules/groups/ui/widgets/groups_widgets.dart
@@ -1,6 +1,4 @@
 export '../dialogs/create_group_dialog.dart';
 export '../../models/create_group_draft.dart';
 export 'groups_list/group_card.dart';
-export 'groups_list/groups_actions.dart';
 export 'groups_list/groups_empty_state.dart';
-export 'groups_list/groups_header.dart';
--- lib/modules/rehearsals/ui/widgets/rehearsal_detail/rehearsal_detail_widgets.dart
+++ lib/modules/rehearsals/ui/widgets/rehearsal_detail/rehearsal_detail_widgets.dart
@@ -1,3 +1,2 @@
-export 'setlist_item_card.dart';
 export '../../pages/setlist_item_dialog.dart';
 export '../../../models/rehearsal_detail_content.dart';
